45018b2fc3065544bf476817d16fffa9cf4fad6e
bubblegum11 <156524626+bubblegum11@users.noreply.github.com>
2024-12-27 07:56:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const noBtn = document.querySelector('.no-btn');
 const wrapperRect = wrapper.getBoundingClientRect();
 const noBtnRect = noBtn.getBoundingClientRect();
 yesBtn.addEventListener('click', () => {
-    question.innerHTML = 'I love you :) Thanks for choosing to be mine.';
+    question.innerHTML = 'Thanks for choosing to be mine.';
 });
 noBtn.addEventListener('mouseover', () => {
     const i = Math.floor(Math.random() * (wrapperRect.width - noBtnRect.width)) + 1;
